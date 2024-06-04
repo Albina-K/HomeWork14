dc0c1d2c1ffb7ef2c521808f8c031e1770952667
Albina <kamalieva.albish@mail.ru>
2024-06-04 17:39:02 +0300

    Добавьте файлы проекта.

---
+++ HomeWork14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeWork14;
+
+namespace PhoneBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  создаём пустой список с типом данных Contact
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            var sortedPhoneBook = phoneBook
+                .OrderBy(s => s.Name)
+                .ThenBy(s => s.LastName);
+
+            foreach (var phone in sortedPhoneBook)
+                Console.WriteLine($"{phone.Name}, {phone.LastName}, {phone.PhoneNumber}, {phone.Email}");
+        }
+    }
+}
